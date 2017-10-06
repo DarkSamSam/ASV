6f5f8c16e0922107c2903576a3d62288192d6912
Sekarski Samuel Joseph-Jean <sekarski@IC-CO-IN-SC201.intranet.epfl.ch>
2017-10-06 16:32:56 +0200

    class definition

--- Complex/include/complex.h
+++ Complex/include/complex.h
@@ -1,11 +1,18 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+#include <iostream>
+
 class Complex
 {
 public:
+	Complex();
+	Complex(double const& a, double const& b);
 	double re();
 	double im();
 	double rho();
 	double theta();
-	ostream& affiche(z);
+	std::ostream& affiche(std::ostream& sortie = std::cout) const;
 	Complex& operator+=(Complex const& z);
 	Complex& operator-=(Complex const& z);
 	Complex& operator*=(Complex const& z);
@@ -17,11 +24,12 @@ public:
 private:
 	double x;
 	double y;
-}
+};
 
 Complex operator+(Complex z1, Complex const& z2);
 Complex operator-(Complex z1, Complex const& z2);
 Complex operator*(Complex z1, Complex const& z2);
 Complex operator/(Complex z1, Complex const& z2);
-ostream& operator<<(ostream& sortie, Complex const& z);
+std::ostream& operator<<(std::ostream& sortie, Complex const& z);
 
+#endif
--- Complex/src/complex.cc
+++ Complex/src/complex.cc
@@ -1,18 +1,129 @@
-double Complex::rho(){}
-double Complex::theta(){}
-double Complex::re(){}
-double Complex::im(){}
-ostream& Complex::affiche(){}
-Complex& Complex::operator+=(Complex const& z){}
-Complex& Complex::operator/=(Complex const& z){}
-Complex& Complex::operator-=(Complex const& z){}
-Complex& Complex::operator*=(Complex const& z){}
-bool Complex::operator==(Complex const& z){}
-bool Complex::operator!=(Complex const& z){}
-void Complex::operator=(Complex const& z){}
+#include "../include/complex.h"
+#include <cmath>
+using namespace std;
 
-Complex& operator+(Complex z1, Complex const& z2){}
-Complex& operator-(Complex z1, Complex const& z2){}
-Complex& operator*(Complex z1, Complex const& z2){}
-Complex& operator/(Complex z1, Complex const& z2){}
-ostream& operator<<(Complex const& z){}
+Complex::Complex(): x(0.0), y(0.0) {}
+Complex::Complex(double const& a, double const& b): x(a), y(b) {}
+
+double Complex::rho()
+{
+	return sqrt(x*x + y*y);
+}
+
+double Complex::theta()
+{
+	double const module(rho());
+	double const precision(1e-15);
+	if(abs(module) < precision)
+	{
+		return 0.0;
+	}
+	else if ((abs(y) < precision) && (x < 0.0))
+	{
+		return M_PI;
+	}
+	else
+	{
+		return 2.0 * atan(y/(x+module));
+	}
+}
+
+double Complex::re()
+{
+	return x;
+}
+
+double Complex::im()
+{
+	return y;
+}
+
+ostream& Complex::affiche(ostream& sortie) const
+{
+	sortie << "(" << x << "," << y << ")";
+	return sortie;
+}
+
+Complex& Complex::operator+=(Complex const& z)
+{
+	x += z.x;
+	y += z.y;
+	return *this;
+}
+
+Complex& Complex::operator/=(Complex const& z)
+{
+	double a = (x*z.x - y*z.x)/(z.x*z.x + z.y*z.y);
+	double b = (x*z.y + y*z.x)/(z.x*z.x + z.y*z.y);
+	x=a;
+	y=b;
+	return *this;
+}
+
+Complex& Complex::operator-=(Complex const& z)
+{
+	x -= z.x;
+	y -= z.y;
+	return *this;
+}
+
+Complex& Complex::operator*=(Complex const& z)
+{
+	double a = x*z.x - y*z.y;
+	double b = x*z.y + y*z.x;
+	x=a;
+	y=b;
+	return *this;
+}
+
+bool Complex::operator==(Complex const& z)
+{
+	if(x == z.x && y == z.y)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool Complex::operator!=(Complex const& z)
+{
+	return !(operator==(z));
+}
+
+void Complex::operator=(Complex const& z)
+{
+	x = z.x;
+	y = z.y;
+}
+
+Complex operator+(Complex z1, Complex const& z2)
+{
+	z1 += z2;
+	return z1;
+}
+
+Complex operator-(Complex z1, Complex const& z2)
+{
+	z1 -= z2;
+	return z1;
+}
+
+Complex operator*(Complex z1, Complex const& z2)
+{
+	z1 *= z2;
+	return z1;
+}
+
+Complex operator/(Complex z1, Complex const& z2)
+{
+	z1 /= z2;
+	return z1;
+}
+
+ostream& operator<<(ostream& sortie, Complex const& z)
+{
+	return z.affiche(sortie);
+}
